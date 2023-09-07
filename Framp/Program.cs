66f1bc5613b24fb1dc5a2e385c3f6a49982618fc
YUJECK <76834558+YUJECK@users.noreply.github.com>
2023-09-07 19:20:11 +0300

    Добавил гитигнор

---
+++ Framp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Framp.Tests;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Framp
+{
+    class Initial
+    {
+        private static void Main()
+        {
+            RenderWindow renderWindow = new(new VideoMode(250, 250), "Framp", Styles.Default);
+
+            EntityMaster entityMaster = new(new List<Entity>());
+            entityMaster.AddEntity(new TestEntity());
+            
+            while (renderWindow.IsOpen)
+            {
+                renderWindow.DispatchEvents();
+                renderWindow.Clear(Color.Black);
+                
+                entityMaster.UpdateEntities();
+                
+                renderWindow.Display();
+            }
+        }
+    }
+}
