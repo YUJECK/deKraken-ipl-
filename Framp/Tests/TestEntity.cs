3d84cfa419a8d09e2c5542b49fd4996fe5cba2ba
YUJECK <76834558+YUJECK@users.noreply.github.com>
2023-09-07 20:34:08 +0300

    Тест возможности двигаться

--- Framp/Tests/TestEntity.cs
+++ Framp/Tests/TestEntity.cs
@@ -5,6 +5,8 @@ namespace Framp.Tests;
 
 public sealed class TestEntity : Entity
 {
+    private Vector2f currentPos;
+    
     public override void OnCreated()
     {
         ComponentsMaster.Add(new Locality(new Vector2f(300, 300), new Vector2f(1, 1)));
@@ -13,5 +15,19 @@ public sealed class TestEntity : Entity
             = new("D:\\GameDev\\Framp\\Framp\\Framp\\Assets\\testTexture.jpg");
         
         ComponentsMaster.Add(new SpriteRenderer(texture));
+    }
+
+    public override void OnUpdate()
+    {
+        currentPos.X += 0.1f;
+        currentPos.Y += 0.1f;
+
+        if (currentPos.X >= 500)
+        {
+            currentPos.X = 0f;
+            currentPos.Y = 0f;
+        }
+        
+        ComponentsMaster.Get<Locality>().Transformable.Position = currentPos;
     }
 }
